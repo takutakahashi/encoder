b370fc9b6438406927e7c4e7f6936ff202f3c78b
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-07-28 22:43:09 +0900

    mod

--- encode.sh
+++ encode.sh
@@ -6,8 +6,8 @@ ffmpeg -y \
   -nostdin \
   -hwaccel vaapi \
   -hwaccel_output_format vaapi \
-  -i $SRC \
+  -i "$SRC" \
   -vf 'format=nv12|vaapi,scale_vaapi=w=1280:h=720' \
   -c:v h264_vaapi -profile 100 -level 40 -qp 23 -aspect 16:9 \
   -c:a copy \
-  $DST
+  "$DST"
