761b7f4ea5764c13ac01b7618a8061dae3f43e82
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-07-29 00:21:39 +0900

    add queue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 PRJ_ROOT=$PWD
 SRC=$1
 DST=$2
 cd $SRC
 while true; do
-  FILENAME=`find . -type f |grep -v "noenc" |head -1 |tail -1`
+  FILENAME=`find . -type f |grep "queue" |head -1 |tail -1`
   if [[ "`dirname $FILENAME`" != "." ]]; then
     mkdir -p $DST/`dirname $FILENAME`
   fi
-  $PRJ_ROOT/encode.sh "$SRC/$FILENAME" "$DST/$FILENAME.mp4"
+  $PRJ_ROOT/encode.sh "$SRC/$FILENAME" "$DST/$FILENAME.mp4" && rm -f "$SRC/$FILENAME"
   sleep 10
 done
 
