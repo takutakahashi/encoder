6ae10a9c6220aa88194d87f8e5f95dd65849efa4
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2020-07-29 00:03:43 +0900

    fix parameter

--- encode.sh
+++ encode.sh
@@ -8,7 +8,8 @@ ffmpeg -y \
   -hwaccel_output_format vaapi \
   -i "$SRC" \
   -vf 'format=nv12|vaapi,scale_vaapi=w=1280:h=720' \
-  -c:v h264_vaapi -profile 100 -level 40 -qp 23 -aspect 16:9 \
+  -b:v 7M -maxrate 7M \
+  -c:v h264_vaapi -aspect 16:9 \
   -c:a copy \
   -bsf:a aac_adtstoasc \
   "$DST"
